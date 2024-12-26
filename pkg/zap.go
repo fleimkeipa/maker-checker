fb0120dfba5f2059ca491271811fc3aa3665cdfb
fleimkeipa <adem_sahin12@hotmail.com>
2024-12-26 15:12:36 +0300

    Add zap logger pkg

---
+++ pkg/zap.go
@@ -0,0 +1,41 @@
+package pkg
+
+import (
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func ZapLogger(log *zap.Logger) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if err := next(c); err != nil {
+				c.Error(err)
+			}
+
+			req := c.Request()
+			res := c.Response()
+
+			fields := []zapcore.Field{
+				zap.Int("status", res.Status),
+				zap.String("method", req.Method),
+				zap.String("uri", req.RequestURI),
+				zap.String("host", req.Host),
+			}
+
+			status := res.Status
+			switch {
+			case status >= 500:
+				log.Error("Server error", fields...)
+			case status >= 400:
+				log.Warn("Client error", fields...)
+			case status >= 300:
+				log.Info("Redirection", fields...)
+			default:
+				log.Info("Success", fields...)
+			}
+
+			return nil
+		}
+	}
+}

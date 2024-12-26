7dd83d74940d5f44aa83c86649173b408f99d26b
fleimkeipa <adem_sahin12@hotmail.com>
2024-12-26 15:13:11 +0300

    Add errors pkg

---
+++ pkg/errors.go
@@ -0,0 +1,37 @@
+package pkg
+
+import "errors"
+
+// Error struct defines a custom error type with an error, status code, and message.
+type Error struct {
+	err        error
+	statusCode int
+	message    string
+}
+
+// NewError creates a new instance of the Error struct.
+func NewError(err error, message string, statusCode int) *Error {
+	if err == nil {
+		err = errors.New("error")
+	}
+	return &Error{
+		err:        err,
+		message:    message,
+		statusCode: statusCode,
+	}
+}
+
+// Error implements the error interface by returning the original error message.
+func (rc *Error) Error() string {
+	return rc.err.Error()
+}
+
+// Message returns the custom error message.
+func (rc *Error) Message() string {
+	return rc.message
+}
+
+// StatusCode returns the associated HTTP status code.
+func (rc *Error) StatusCode() int {
+	return rc.statusCode
+}
